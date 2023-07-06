e5aecb92798cfd0c047bb65c22c213bb65c26ef2
Annaerith <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-07-06 10:26:43 +0200

    useEffect dependency fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,19 +4,21 @@ import { PropTypes } from 'prop-types';
 import css from './Modal.module.css';
 
 export const Modal = ({ url = 'no url', alt = 'no alt' }) => {
-  const { setModalIndex } = useAppContext();
-  const handleKeyUp = e => {
-    if (e.key === 'Escape') setModalIndex(-1);
-  };
+  const { modalIndex, setModalIndex } = useAppContext();
+
   const closeModal = e => {
     if (e.target.className === css.Overlay) setModalIndex(-1);
   };
+
   useEffect(() => {
+    const handleKeyUp = e => {
+      if (e.key === 'Escape') setModalIndex(-1);
+    };
     document.addEventListener('keyup', handleKeyUp);
     return () => {
       document.removeEventListener('keyup', handleKeyUp);
     };
-  }, []);
+  }, [modalIndex, setModalIndex]);
 
   return (
     <div className={css.Overlay} onClick={closeModal}>
